60ab6fd06495208ffd09b1a09d01384c2277eec2
Samyak Kakatur <107428179+therealsamyak@users.noreply.github.com>
2024-04-23 06:19:17 +0000

    updated function definitions *check equality of pointers with function arguments

--- sysproc.c
+++ sysproc.c
@@ -14,21 +14,46 @@ sys_fork(void)
 }
 
 int
-sys_exit(int status)
+sys_exit(void)
 {
+  int status;
+  if(argint(0, &status) < 0)
+    return -1;
   exit(status);
   return 0;  // not reached
 }
 
+
 int
-sys_wait(int* status)
+sys_wait(void)
 {
+  int* status;
+  if (argptr(0, (void*)&status, sizeof(*status)) < 0) {
+    return -1; //returns -1 if no child exists
+  }
   return wait(status);
 }
 
-int sys_waitpid(int pid, int* status, int options) 
+int
+sys_waitpid(void)
 {
-  return waitpid(pid, status, options);
+  int pid;
+  int* status;
+  int options;
+
+  if (argint(0, &pid) < 0) {
+    return -1;
+  }
+
+  if (argptr(1, (void*)&status, sizeof(status)) < 0) {
+    return -1;
+  }
+
+  if (argint(2, &options) < 0) {
+    return -1;
+  }
+
+  return waitpid(pid,status,options);
 }
 
 int
